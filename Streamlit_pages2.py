62b2a0cc834370414a6bb22ba64f8bd3e11a7137
RayJohnson2 <30327121+RayJohnson2@users.noreply.github.com>
2022-02-17 15:41:34 +0100

    Add files via upload

--- Streamlit_pages2.py
+++ Streamlit_pages2.py
@@ -13,6 +13,7 @@ def CB_Slider1():
     
 def home():
     st.write("Welcome to home page")
+    st.session_state
     st.checkbox("Check me", value=st.session_state.check1, key='check1')
     if st.button("Click Home"):
         st.write("Welcome to home page")
@@ -20,11 +21,13 @@ def home():
 def slider():
     #st.title('this is a different page. One with a slider.')
     st.write("Welcome to the slider page")
+    st.session_state
     slide1 = st.slider('this is a slider',min_value=0,max_value=15,value=st.session_state.slider1 ,key='slider1' )
     slide1
     
 def contact():
     st.title("Welcome to contact page")
+    st.session_state
     if st.button("Click Contact"):
         st.write("Welcome to contact page")
 
