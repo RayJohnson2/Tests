06b229b71fb29eb2d029f681fd1b7190ded8227e
RayJohnson2 <30327121+RayJohnson2@users.noreply.github.com>
2022-02-17 15:08:43 +0100

    Add files via upload

--- Streamlit_pages2.py
+++ Streamlit_pages2.py
@@ -1,11 +1,11 @@
 import streamlit as st
+
 my_page = st.sidebar.radio('Page Navigation', ['Home', 'Slider', 'Contact'])
 
 if 'slider1' not in st.session_state:
     st.session_state.slider1 = 0
 if 'check1' not in st.session_state:
     st.session_state.check1 = False
-
 
 def CB_Slider1():
     #st.session_state.slider1 = slide1
@@ -17,29 +17,16 @@ def home():
     if st.button("Click Home"):
         st.write("Welcome to home page")
 
-
 def slider():
     #st.title('this is a different page. One with a slider.')
     st.write("Welcome to the slider page")
     slide1 = st.slider('this is a slider',min_value=0,max_value=15,value=st.session_state.slider1 ,key='slider1' )
     slide1
     
-
-
 def contact():
     st.title("Welcome to contact page")
     if st.button("Click Contact"):
         st.write("Welcome to contact page")
-        
-##if my_page == 'page 1':
-##    st.title('here is a page')
-##    button = st.button('a button')
-##    if button:
-##        st.write('clicked')
-##else:
-##    st.title('this is a different page. One with a slider.')
-##    slide = st.slider('this is a slider')
-##    slide
 
 if my_page == 'Home':
     home()
